4c3966c3faa8195679f4befcdcfe02d288d77fe3
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-02-03 17:42:00 +0600

    the deparinc of var

---
+++ js class 1/naming.js
@@ -0,0 +1,33 @@
+// // 1. no keyword in variable name 
+
+// var false =45;
+// var var = 50;
+
+// // 2. no space or gap in variable name 
+// var is happy =  false;
+// var ishappy = true;
+
+// // 3. no quote
+// var "address" = "kochu khet";
+
+// // 4. can not start a number. but number other than the first letter is allowed
+// var money = 45;
+// var mony2 = 55;
+// var 3money = 88;
+
+
+// // 5. name is case sensitive
+
+// address
+// address
+// ADDRESS
+// addRess
+// ADDress 
+
+// // 6. how to write a long variable name
+
+// var my current home address = "Andorkilkl Bandorbon";
+// var my_current_home_address = "Andorkilkl Bandorbon"; // snake case
+// var mycurrenthomeaddress = "Andorkilkl Bandorbon";
+// var myCurrentHomeAddress = "Andorkilkl Bandorbon"; // camel case
+// var MyCurrentHomeAddress = "Andorkilkl Bandorbon"; // Pascal case
