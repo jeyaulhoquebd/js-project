bb055571ea647429673200b9b5c2fc210b17818f
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-02-03 18:39:52 +0600

    13-8

--- js class 1/number.js
+++
@@ -1,5 +0,0 @@
-var orangePrice = 20;
-
-var chocolate = 0.5;
-
-console.log(orangePrice);
---
+++ number.js
@@ -0,0 +1,28 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+
+var applePrice = parseInt('20') ; /*parseInt এর ভিতর যা যে সংখ্যা লিখবো তা সেটাকে int হিসেবে দরে নিবে। অবশ্যই সংখ্যা দিতে হবে এবং যদি আমি ২২.৫ লিখি 
+তাহলেও এটা পূর্ণ সংখ্যা ২২ হয়ে যাবে */
+
+var applePricer = parseFloat('22.5'); //parseFloat দশমিক সংখ্যার জন্য
+
+// console.log( orangePrice);
+
+// console.log( applePrice);
+
+// console.log(orangePrice + applePrice);
+
+console.log(applePricer);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+
+console.log(total.toFixed(3)); //.toFixed(3) মানে হলো রান করলে দশমিকের পর ৩টা সংখ্যা হবে 
+
+
+const num = -50 / 0;
+console.log(num); // এটা পরে বুজবো ১৩.৯ মডিউল
+
+
+
