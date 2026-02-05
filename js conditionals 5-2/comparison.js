fc2a4f024c611d6ec31968018af8333a47eae31a
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-02-05 20:33:44 +0600

    git commit

---
+++ js conditionals 5-2/comparison.js
@@ -0,0 +1,34 @@
+// console.log(5>10);
+// console.log(5 < 10);
+// console.log(10 == 10);
+// console.log(10 >= 10);
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+// console.log(10 <= 10);
+// console.log(100 <= 10);
+
+// console.log(10 == 10);
+// console.log(10 == 10.1);
+
+//------------------------
+
+var peyaraPrice = 50;
+var applePrice = 260;
+
+// console.log(peyaraPrice < applePrice);
+
+// console.log(applePrice < peyaraPrice);
+
+
+/**
+ * 
+ * ==
+ * ===
+ */
+
+// console.log(10 == 10)
+// console.log(10 == '10')
+// console.log(10 === '10')
+
+// console.log(10 != '10'); 
+console.log(10 !== '10');  // এখানে ২টা == দিলে সে '' এটার ভিতরে কি আছে তাও দেখবে 
