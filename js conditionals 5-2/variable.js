5b86c5adeaa2e58b89d7f0c1e921e61740f32830
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-02-05 19:39:34 +0600

    git commit

---
+++ js conditionals 5-2/variable.js
@@ -0,0 +1,34 @@
+/**
+ * যা যা শিখলাম 
+ * 
+ * VARIABLE ==> Container
+ */
+// Number Type Variable 
+var sunglassPrice = 80;
+
+//string type Variable
+var subject = "EEE";
+
+// Boolean type variable 
+var passed = true;
+
+/**
+ * Naming Convention  কিছু রুলস
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. prefer camelCase  যেমন lastExamResult
+ */
+
+/**
+ * OPERATION
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN?
+ * 4. null 
+ * 5. Number 
+ * 
+ * এগুলা হলো গত পর্বের পুনরাবির্তি
+ */ 
