f545e5b94cbb3d7fa54e215ce030d8ee4f9982a5
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-02-06 23:56:01 +0600

    git push

---
+++ js conditionals 5-2/multiple.js
@@ -0,0 +1,35 @@
+const salary = 21000;
+const isBCS = true;
+const hascar = false;
+const height = 68;
+
+// if (salary > 20000 && height > 66){
+//     console.log('su---patro');
+// }
+
+// else{
+//     console.log('onno patro khuji');
+// }
+
+// if (salary > 25000 || height > 72){
+//     console.log('eso baba khobul ')
+// }
+
+// else {
+//     console.log('vaag tui mokbul') 
+// }
+
+
+//more and more condition
+if (salary > 25000 || height > 72 || isBCS == true){
+    console.log('eso baba khobul ')
+}
+
+else {
+    console.log('vaag tui mokbul') 
+}
+
+//----------------------COMPLEX CONDITION-----------------
+if((salary > 25000 && hascar == true) || isBCS == true){
+    console.log('tomar 14 gosti raji ')
+}
