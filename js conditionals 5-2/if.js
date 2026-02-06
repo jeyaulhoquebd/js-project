6cf37f11ca2610f9a4bea5ab0496fcddb4233f42
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-02-06 12:20:41 +0600

    git commit

---
+++ js conditionals 5-2/if.js
@@ -0,0 +1,51 @@
+/**
+ * Tui jodi amake khawas 
+ *  taile ami asbo
+ * 
+ * ------------------
+ * if you buy food for me 
+ *     i will come 
+ * 
+ * 
+ * -------------
+ * 
+ * if briyani price less than 300 taka i will buy
+ * 
+ * --------STRUCTURE----------
+ * if (condition){
+ * 
+ * }
+ */
+
+// if(3 < 10){
+//     console.log('I have a smaller value');
+// }
+
+// else{
+//     console.log('I have big value ');
+// }
+
+// var biryaniPrice = 250 ;
+
+// if (biryaniPrice < 300){
+//     console.log('mama give me some biryani')
+// }
+
+/**
+ * if (condition){
+ *              execute code if the condition is false
+ * }
+ * else {
+ *      execute code if the condition is false 
+ * }
+ */
+
+var weight = 20;
+
+if (weight <= 20){
+    console.log('I will carry it by myself');
+}
+
+else{
+    console.log('I will rent a rickshaw');
+}
