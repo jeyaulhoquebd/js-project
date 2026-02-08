239ba00a14fb20ebe7376f470f6bcca1b5b3e982
jeyaulhoquebd <jeyaulhoque2025@gmail.com>
2026-02-08 16:52:13 +0600

    git commit

---
+++ js array/array.js
@@ -0,0 +1,14 @@
+// const num1 = 12;
+// const num2 = 22;
+// const num3 = 442;
+// const num4 = 446;
+
+// const number = 242409808023849; এখানে কিছু বুঝা যাচ্ছে না।
+
+// (), {}, []
+
+const numbers = [12, 22, 54, 67];
+const friends = ['Abule', 'bubul', 'cabul', 'dabul', 'bulbul'];
+const passed = [true, false, true, false];
+const mixedArray = [12, 'Alahbad', true, 45.909]; // এটা ব্যবহার করা কম হয়
+
